a37e227c6ac30891f3470268af802e668bd5cf39
haeunko <hekoh99@naver.com>
2022-07-15 16:29:26 +0900

    setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express(); 
 
 app.get("/", (req, res) => {
-    res.send("UMC server page");
+    res.send("UMC server page!");
 });
 
 app.listen(3000, () => console.log("🚀successfully started!"));
