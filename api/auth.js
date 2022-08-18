f8c316f6f3ebf6d563d32f55acabeb88e0adcfad
haeunko <hekoh99@naver.com>
2022-08-18 11:10:22 +0900

    jwt token

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 var status = require("../status.js");
@@ -6,6 +7,7 @@ const db = require("../config/database.js");
 const mysql = require("mysql2");
 const { User } = require("../model/model.js");
 const { json } = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", function (req, res) {
   var salt = bcrypt.genSaltSync(10);
@@ -42,18 +44,27 @@ router.post(
       isValid = false;
       validationError.errors.password = { message: "Password is required!" };
     }
-    if (!isValid) return res.json(util.successFalse(validationError));
+    if (!isValid) return res.json(status.parseError(validationError));
     else next();
   },
   function (req, res, next) {
     User.findOne({ where: { username: req.body.username } })
       .then((row) => {
-        console.log(row);
+        const payload = {
+          username: row.username,
+        };
+        const secretKey = process.env.JWT_SECRET;
+        const opts = { expiresIn: 60 * 60 * 24 };
+        jwt.sign(payload, secretKey, opts, function (err, token) {
+          if (err) return res.json(status.parseError(err));
+          // console.log(token);
+          res.json(status.success(token));
+        });
+        // res.json(status.success("test"));
       })
       .catch(function (err) {
         res.json(status.failure(err, "login failed"));
       });
-    res.json(status.success("test"));
   }
 );
 
