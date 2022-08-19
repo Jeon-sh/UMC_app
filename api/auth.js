17753f821245eab85086400b90fb3b7abeb38ff9
haeunko <hekoh99@naver.com>
2022-08-19 11:04:40 +0900

    verify password

--- api/auth.js
+++ api/auth.js
@@ -2,30 +2,20 @@ require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 var status = require("../status.js");
-var bcrypt = require("bcrypt-nodejs");
-const db = require("../config/database.js");
+var bcrypt = require("bcrypt");
 const mysql = require("mysql2");
 const { User } = require("../model/model.js");
 const { json } = require("body-parser");
 const jwt = require("jsonwebtoken");
 
-router.post("/register", function (req, res) {
-  var salt = bcrypt.genSaltSync(10);
+router.post("/register", async function (req, res) {
   var data = req.body;
   var email = data.email;
   var password = data.password;
-  var hashPwd = bcrypt.hashSync(password, salt);
+  const hashPwd = await bcrypt.hash(password, 10);
   var username = data.username;
-  db.query(
-    `insert into users(username, password, email) value(?, ?, ?)`,
-    [username, hashPwd, email],
-    function (err, result) {
-      if (err) throw err;
-      console.log(result);
-      res.json(status.success(username));
-    }
-  );
-  return router;
+  const user = await User.create({ username, password: hashPwd, email });
+  return res.json(status.success(user.username));
 });
 
 router.post(
@@ -47,20 +37,27 @@ router.post(
     if (!isValid) return res.json(status.parseError(validationError));
     else next();
   },
-  function (req, res, next) {
+  async function (req, res, next) {
     User.findOne({ where: { username: req.body.username } })
       .then((row) => {
-        const payload = {
-          username: row.username,
-        };
-        const secretKey = process.env.JWT_SECRET;
-        const opts = { expiresIn: 60 * 60 * 24 };
-        jwt.sign(payload, secretKey, opts, function (err, token) {
-          if (err) return res.json(status.parseError(err));
-          // console.log(token);
-          res.json(status.success(token));
+        bcrypt.compare(req.body.password, row.password, function (err, result) {
+          if (err) return res.json(status.failure(err, "bcrypt error"));
+          else if (!result)
+            return res.json(status.failure(err, "wrong password"));
+          else {
+            const payload = {
+              username: row.username,
+            };
+            const secretKey = process.env.JWT_SECRET;
+            const opts = { expiresIn: 60 * 60 * 24 };
+            jwt.sign(payload, secretKey, opts, function (err, token) {
+              if (err) return res.json(status.parseError(err));
+              // console.log(token);
+              res.json(status.success(token));
+            });
+            // res.json(status.success("test"));
+          }
         });
-        // res.json(status.success("test"));
       })
       .catch(function (err) {
         res.json(status.failure(err, "login failed"));
